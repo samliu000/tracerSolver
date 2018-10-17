e34e2cc22198afb24c5ab8d668b3c5300e72d2cc
samliu000 <44206874+samliu000@users.noreply.github.com>
2018-10-16 23:07:11 -0700

    Update Main.java

--- src/tracersolver/Main.java
+++ src/tracersolver/Main.java
@@ -1,59 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tracersolver;
-
-import java.util.Scanner;
-import java.util.Vector;
-import static tracerSolver.TracerSolver.convertToString;
-import static tracerSolver.TracerSolver.pointToDirectionConverter;
-
-/**
- *
- * @author nickw
- */
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Number of Rows: ");
-        int row = in.nextInt();
-        System.out.println("Number of Cols: ");
-        int col = in.nextInt();
-        System.out.println("Enter starting row");
-        int startRow = in.nextInt();
-        System.out.println("Enter starting column");
-        int startCol = in.nextInt();
-        tracerSolver.TracerSolver grid = new tracerSolver.TracerSolver(row, col, startRow, startCol);
-
-        System.out.println("Would you like to remove any blocks? N for no; Any other key for Yes");
-        while(!in.next().toLowerCase().equals("n")) {
-                System.out.print("Row: ");
-                int removedRow = in.nextInt();
-                System.out.print("Column: ");
-                int removedCol = in.nextInt();
-                if(removedRow == startRow && removedCol == startCol) {
-                        System.out.println("You cannot remove the starting position");
-                } else {
-                        grid.removeSpace(removedRow, removedCol);
-                }
-                System.out.println("Another?");
-        }
-        if(grid.solveTracer(startRow, startCol)) {
-                Vector<tracerSolver.Point> listOfMoves = grid.getDirections();
-                Vector<String> stringForm = pointToDirectionConverter(listOfMoves, startRow, startCol);
-                int[][] numberedGrid = new int[row][col];
-                System.out.println(stringForm.toString());
-                for(int i = 1; i < listOfMoves.size()+1; i++) {
-                        int row1 = listOfMoves.elementAt(i-1).x;
-                        int col1 = listOfMoves.elementAt(i-1).y;
-                        numberedGrid[row1][col1] = i;
-                }
-                convertToString(numberedGrid);
-        } else {
-                System.out.println("Puzzle could not be solved");
-        }
-        in.close();
-    }
-}
+public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Begin?");
+		while(!in.next().toLowerCase().equals("n")) {
+			
+			//prompts for constuctor parameters
+			System.out.println("Number of Rows: ");
+			int row = in.nextInt();
+			System.out.println("Number of Cols: ");
+			int col = in.nextInt();
+			System.out.println("Enter starting row");
+			int startRow = in.nextInt();
+			System.out.println("Enter starting column");
+			int startCol = in.nextInt();
+			
+			//constructs object
+			tracerSolver grid = new tracerSolver(row, col, startRow, startCol);
+			
+			//repeatedly asks the user if there are any spaces to mark
+			System.out.println("Would you like to remove any blocks? N for no; Any other key for Yes");
+			while(!in.next().toLowerCase().equals("n")) {
+				System.out.print("Row: ");
+				int removedRow = in.nextInt();
+				System.out.print("Column: ");
+				int removedCol = in.nextInt();
+				//removing starting position is not allowed
+				if(removedRow == startRow && removedCol == startCol) {
+					System.out.println("You cannot remove the starting position");
+				} else {
+					grid.removeSpace(removedRow, removedCol);
+				}
+				System.out.println("Another? N for no; Any other key for Yes");
+			}
+			
+			//start timer
+			double startTime = System.nanoTime();
+			
+			//attempt to solve puzzle
+			if(grid.solveTracer(startRow, startCol)) {
+				Vector<Point> listOfMoves = grid.getDirections();
+				Vector<String> stringForm = pointToDirectionConverter(listOfMoves, startRow, startCol);
+				
+				//prints out solution steps in the form of directional movement
+				System.out.println(stringForm.toString());
+				
+				//constructs and initializes a grid to give a visual representation of the solution
+				int[][] numberedGrid = new int[row][col];
+				for(int i = 1; i < listOfMoves.size() + 1; i++) {
+					int row1 = listOfMoves.elementAt(i - 1).getX();
+					int col1 = listOfMoves.elementAt(i - 1).getY();
+					numberedGrid[row1][col1] = i;
+				}
+				convertToString(numberedGrid, row, col);
+				
+				//stop timer and print out how long program took to solve puzzle
+				double duration = System.nanoTime() - startTime;
+				System.out.println();
+				System.out.println("Solved in " + duration/1000000000 + " seconds");
+				System.out.println();
+				
+				//prompts user if they want to solve another puzzle
+				System.out.println("Another? N for no; Any other key for Yes");
+			} 
+			//if puzzle could not be solved, indicate so and reprompt user
+			else {
+				System.out.println("Puzzle could not be solved");
+				System.out.println("Another? N for no; Any other key for Yes");
+			}
+		}
+		
+		System.out.println("Done");
+		
+		in.close();
+	}
